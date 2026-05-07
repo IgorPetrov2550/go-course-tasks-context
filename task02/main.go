b7905f279b033a01de86928e4c9f66bdbb69c765
IgorPetrov2550 <Igorpetrov2550@gmail.com>
2026-05-08 00:43:51 +0500

    Task02

--- task02/main.go
+++ task02/main.go
@@ -49,14 +49,39 @@ import (
 
 // TODO: напиши функцию downloadFile(ctx context.Context, url string) (string, error)
 
+func downloadFile(ctx context.Context, url string) (string, error) {
+	select {
+	case <-time.After(2 * time.Second):
+		return "содержимое " + url, nil
+	case <-ctx.Done():
+		return "", ctx.Err()
+	}
+}
+
 func main() {
 	// Вызов 1: таймаут 3 секунды - должен успеть
 	// TODO: создай контекст с таймаутом 3s, вызови downloadFile, выведи результат
+	start1 := time.Now()
+	ctx1, cancel1 := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel1()
 
+	//res1, err1 := downloadFile(ctx1, "https://example.com/file.txt")
+	if res1, err1 := downloadFile(ctx1, "https://example.com/file.txt"); err1 != nil {
+		fmt.Printf("ошибка: %v (за %v)\n", err1, time.Since(start1))
+	} else {
+		fmt.Printf("скачано: %v (за %v)\n", res1, time.Since(start1))
+	}
 	// Вызов 2: таймаут 1 секунда - не успеет
 	// TODO: создай контекст с таймаутом 1s, вызови downloadFile, выведи ошибку
+	start2 := time.Now()
 
-	_ = context.Background
-	_ = fmt.Println
-	_ = time.Second
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel2()
+
+	if res2, err2 := downloadFile(ctx2, "https://example.com/file.txt"); err2 != nil {
+		fmt.Printf("ошибка: %v (за %v)\n", err2, time.Since(start2))
+	} else {
+		fmt.Printf("скачано: %v (за %v)\n", res2, time.Since(start2))
+	}
+
 }
